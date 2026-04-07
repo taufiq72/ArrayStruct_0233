94ce068752709c46265ec6711e7681d0eb7bd8ce
taufiq72 <taufiqq1207@gmail.com>
2026-04-07 18:40:00 +0700

    mengisi data

--- structdetailalamat.cpp
+++ structdetailalamat.cpp
@@ -18,4 +18,14 @@ int main()
 {
     //Deklarasi variabel struct
     Orang mhs;
+    //Mengisi Data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "Isikan data alamat : ";
+    cin >> mhs.alamat.desa; 
+    cout << "Isikan data alamat : ";
+    cin >> mhs.alamat.kota; 
+    cout << "Isikan data umur : ";
+    cin >> mhs.umur; 
+    cout << endl;
 }
