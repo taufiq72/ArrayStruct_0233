997a63fe233518a090359668a06d9112d5be62de
taufiq72 <taufiqq1207@gmail.com>
2026-04-07 18:21:06 +0700

    cara akses

--- Array.cpp
+++ Array.cpp
@@ -8,4 +8,10 @@ int main()
     //Deklarasi array tanpa inisialisasi
     int datamu[5];
 
+    //Cara akses (membaca atau mengambil isi array)
+    cout << "Isi data kedua : " << dataku[1] << endl;
+    //Mengisi array
+    dataku[1] = 200;
+    cout << "Isi data kedua setelah diubah : "<< dataku[1] << endl;
+
 }
