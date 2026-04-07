19ab10b5ddea027b56e483d1ae1f8c00278a96c5
taufiq72 <taufiqq1207@gmail.com>
2026-04-07 18:28:59 +0700

    Mengisi data

--- struct.cpp
+++ struct.cpp
@@ -12,4 +12,11 @@ int main()
 {
     //Deklarasi variabel struct
     Orang mhs;
+    //Mengisi data
+    cout << "Isikan data nama : ";
+    cin >> mhs.nama;
+    cout << "Isikan data alamat : ";
+    cin >> mhs.alamat; 
+    cout << "Isikan data umur : ";
+    cin >> mhs.umur; 
 }
