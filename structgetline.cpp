8612306e73c6de33c1b59a2de955a017fc5ce086
taufiq72 <taufiqq1207@gmail.com>
2026-04-07 18:53:53 +0700

    menambahkan library string

---
+++ structgetline.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct detailalamat
+{
+    string desa;
+    string kota;
+};
+
+struct Orang
+{
+    string nama;
+    detailalamat alamat;
+    int umur;
+};
+
+int main()
+{
+    //Deklarasi variabel struct
+    Orang mhs;
+    //Mengisi Data
+    cout << "Isikan data nama : ";
+    getline(cin, mhs.nama);
+    cout << "Isikan data alamat : ";
+    getline(cin, mhs.alamat.desa); 
+    cout << "Isikan data alamat : ";
+    getline(cin, mhs.alamat.kota); 
+    cout << "Isikan data umur : ";
+    cin >> mhs.umur; 
+    cout << endl;
+
+    //menampilkan data
+    cout << "Data Tampil" << endl;
+    cout << "Nama :" << mhs.nama << endl;
+    cout << "Desa :" << mhs.alamat.desa << endl;
+    cout << "Kota :" << mhs.alamat.kota << endl;
+    cout << "Umur : " << mhs.umur << endl;
+}
